bbc44b8e6b239d479af42cce9d83f849a856cf04
RobinRS <robinschleser0608@gmail.com>
2025-05-24 14:07:20 +0200

    Fix

--- components/target-upload.tsx
+++ components/target-upload.tsx
@@ -351,7 +351,7 @@ export function TargetUpload ({ shootData, set }: { shootData: any, set: React.D
 
                       </div>
                     ))}
-                  {newResult.ergebnis.length < 4 && <PlusCircle className="w-4 h-4" onClick={() => {
+                  {Object.keys(newResult).length > 0 && newResult?.ergebnis?.length < 4 && <PlusCircle className="w-4 h-4" onClick={() => {
                     const ergebnis = newResult.ergebnis || [];
                     ergebnis.push(0);
                     setNewResult({ ...newResult, ergebnis });
