fdc5dfbca0052b9dbcfb4e83b6a759e42f6a1d59
RobinRS <robinschleser0608@gmail.com>
2025-05-28 18:52:47 +0200

    Fix date

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,13 +6,15 @@ export function cn (...inputs: ClassValue[]) {
 }
 
 export function dateFromString (dateString: string): Date {
-  const parts = dateString.split(".");
-  if (parts.length !== 3) {
-    throw new Error("Invalid date format. Expected format: DD.MM.YYYY");
+  if (dateString.includes(".")) {
+    const parts = dateString.split(".");
+    if (parts.length !== 3) {
+      throw new Error("Invalid date format. Expected format: DD.MM.YYYY");
+    }
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
   }
-  const day = parseInt(parts[0], 10);
-  const month = parseInt(parts[1], 10) - 1;
-  const year = parseInt(parts[2], 10);
-
-  return new Date(year, month, day);
+  return new Date();
 }
